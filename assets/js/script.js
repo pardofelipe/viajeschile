a43fcee450d26741c01fc8a1d783a4042a16d47d
Felipe Pardo <felipepardo@MacBook-Pro-de-Felipe.local>
2022-04-05 08:47:40 -0400

    formulario contacto

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,20 +18,9 @@ $(function(){
 });
 
 //smoothScroll
-window.scroll({
-    top: 2500, 
-    left: 0, 
-    behavior: 'smooth'
-  });
-  
-  // Scroll certain amounts from current position 
-  window.scrollBy({ 
-    top: 100, // could be negative value
-    left: 0, 
-    behavior: 'smooth' 
-  });
-  
-  // Scroll to a certain element
-  document.querySelector('.hello').scrollIntoView({ 
-    behavior: 'smooth' 
-  });
+function scrollToAnchor(aid){
+  const destination = $("section");
+    $('html,body').animate({
+      scrollTop: destination.offset().top
+    },'slow');
+}
