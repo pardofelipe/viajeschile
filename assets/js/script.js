30e9a8d7f284e4f50085e1ceba7d1d07613e33de
Felipe Pardo <felipepardo@MacBook-Pro-de-Felipe.local>
2022-04-04 22:12:51 -0400

    Carrusel y documentacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,22 @@ $(function(){
         $('.card-text').toggle(1500)
     })
 });
+
+//smoothScroll
+window.scroll({
+    top: 2500, 
+    left: 0, 
+    behavior: 'smooth'
+  });
+  
+  // Scroll certain amounts from current position 
+  window.scrollBy({ 
+    top: 100, // could be negative value
+    left: 0, 
+    behavior: 'smooth' 
+  });
+  
+  // Scroll to a certain element
+  document.querySelector('.hello').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
